4ca8fef4a9860ca4a57add5aadd1ec818453d456
harry <nuintuition@gmail.com>
2020-05-03 21:51:17 +0800

    新增 MS SQL 連線

---
+++ mssqldb.py
@@ -0,0 +1,10 @@
+import pyodbc
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+server = 'server ip'
+database = 'DevDB'
+username = 'sa'
+password = 'password'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
